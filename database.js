9a6d4a8996163a767603759d101a309087a31c2c
Ajaykumar93922 <123532067+Ajaykumar93922@users.noreply.github.com>
2024-09-02 16:13:10 +0530

    Update database connection for Supabase

--- database.js
+++ database.js
@@ -1,20 +1,20 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "sql6.freemysqlhosting.net",
-  database: "sql6697610",
-  user: "sql6697610",
-  password: "@Ajaykumar93922",
-});
+// const connection = mysql.createConnection({
+//   host: "sql6.freemysqlhosting.net",
+//   database: "sql6697610",
+//   user: "sql6697610",
+//   password: "@Ajaykumar93922",
+// });
 
-connection.connect((error) => {
-  if (error) {
-    console.log("Unable to connect to the server");
-  } else {
-    console.log("Connection successful");
-  }});
+// connection.connect((error) => {
+//   if (error) {
+//     console.log("Unable to connect to the server");
+//   } else {
+//     console.log("Connection successful");
+//   }});
 
-module.exports = connection;
+// module.exports = connection;
 
 // const mysql = require("mysql");
 
@@ -33,3 +33,44 @@ module.exports = connection;
 //   }});
 
 // module.exports = connection;
+
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   user: 'postgres.cfoseljvzrbzkowevqpu', // Replace with your actual user
+//   host: 'aws-0-ap-south-1.pooler.supabase.com',
+//   database: 'postgres',
+//   password: '@Ajaykumar93922', // Replace with your actual password
+//   port: 6543,
+// });
+
+// client.connect((err) => {
+//   if (err) {
+//     console.error('Unable to connect to the server:', err.message);
+//   } else {
+//     console.log('Connection successful');
+//   }
+// });
+
+// module.exports = client;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432, // Default port for PostgreSQL
+});
+
+client.connect((err) => {
+  if (err) {
+    console.error('Unable to connect to the server:', err.message);
+  } else {
+    console.log('Connection successful');
+  }
+});
+
+module.exports = client;
+
