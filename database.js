fb9012c1de1eff8a25ac2b7e3e2645600bba6877
Ajaykumar93922 <123532067+Ajaykumar93922@users.noreply.github.com>
2024-09-02 14:55:02 +0530

    Initial commit

--- database.js
+++ database.js
@@ -1,22 +1,22 @@
+//const mysql = require("mysql");
+
+// const connection = mysql.createConnection({
+//   host: "sql6.freemysqlhosting.net",
+//   database: "sql6697610",
+//   user: "sql6697610",
+//   password: "@Ajaykumar93922",
+// });
+
+// connection.connect((error) => {
+//   if (error) {
+//     console.log("Unable to connect to the server");
+//   } else {
+//     console.log("Connection successful");
+//   }});
+
+// module.exports = connection;
+
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: "sql6.freemysqlhosting.net",
-  database: "sql6697610",
-  user: "sql6697610",
-  password: "@Ajaykumar93922",
-});
-
-connection.connect((error) => {
-  if (error) {
-    console.log("Unable to connect to the server");
-  } else {
-    console.log("Connection successful");
-  }});
-
-module.exports = connection;
-
-/*const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -32,4 +32,4 @@ connection.connect((error) => {
     console.log("Connection successful");
   }});
 
-module.exports = connection;*/
+module.exports = connection;
